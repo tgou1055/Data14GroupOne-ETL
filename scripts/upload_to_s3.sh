75d5f7d10f23f8ca04103116b775dfdfd5ce173f
Yu Hou <hy1201750@gmail.com>
2024-07-06 14:30:40 +1000

    update

--- scripts/upload_to_s3.sh
+++ scripts/upload_to_s3.sh
@@ -6,13 +6,23 @@ set -e
 # Parameters
 SCRIPTS_BUCKET=$1
 
+# Create a temporary directory for the Lambda function
+TEMP_DIR=$(mktemp -d)
+cp scripts/lambda/raw2staging.py ${TEMP_DIR}
+
+# Zip the Lambda function
+echo "Zipping Lambda function..."
+cd ${TEMP_DIR}
+zip raw2staging.zip raw2staging.py
+cd -
+
 # Upload Glue Job to S3
 echo "Uploading Glue Job scripts to S3..."
 aws s3 sync scripts/gluejob/ s3://${SCRIPTS_BUCKET}/gluejob/
 
-# Upload Lambda function to S3
-echo "Uploading Lambda function to S3..."
-aws s3 cp scripts/lambda/raw2staging.py s3://${SCRIPTS_BUCKET}/lambda/raw2staging.py
+# Upload zipped Lambda function to S3
+echo "Uploading zipped Lambda function to S3..."
+aws s3 cp ${TEMP_DIR}/raw2staging.zip s3://${SCRIPTS_BUCKET}/lambda/raw2staging.zip
 
 # Upload Step Function definition to S3
 echo "Uploading Step Function definition to S3..."
