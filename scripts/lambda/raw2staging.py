13d09932ae92b51ad4e5b49bc70da759890bc805
sam <hy1201750@gmail.com>
2024-07-08 03:30:51 +1000

    update

--- scripts/lambda/raw2staging.py
+++ scripts/lambda/raw2staging.py
@@ -7,17 +7,17 @@ s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    print(event)
-    records = event['Records']
     source_bucket = os.environ['SOURCE_BUCKET']
     destination_bucket = os.environ['DESTINATION_BUCKET']
-    
-    for record in records:
-        key = record['s3']['object']['key']
+    # List all objects in the source bucket
+    objects = s3_client.list_objects_v2(Bucket=source_bucket)
+
+    for obj in objects.get('Contents', []):
+        key = obj['Key']
         copy_source = {'Bucket': source_bucket, 'Key': key}
         
         # if gz file, first gunzip, other file types will be just copied
-        if key.endswith('.gz'):
+        if key.endswith('.csv.gz'):
             #first download to the tmp folder
             local_gz_file = f'/tmp/{key.split("/")[-1]}'
             s3_client.download_file(source_bucket, key, local_gz_file)
@@ -32,8 +32,10 @@ def lambda_handler(event, context):
             dest_key = f'{local_csv_file.split("/")[-1]}'
             s3_client.upload_file(local_csv_file, destination_bucket, dest_key)
 
-        else:
+        elif key.endswith('.csv'):
             dest_key = f'{key.split("/")[-1]}' 
             s3.meta.client.copy(copy_source, destination_bucket, dest_key)
+        else:
+            print(f"raw2staging: {key} doesn't end with .csv.gz nor .csv")
         
         
