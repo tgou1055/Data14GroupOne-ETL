8f1f4e5d6454b8993a2da090d10d869fa96fc861
lwkers <69199071+lwkers@users.noreply.github.com>
2024-07-07 17:30:13 +1000

    Update raw2staging.py
    move raw bucket files to staging bucket files
    if gz file, first gunzip, other file types will be just copied

--- scripts/lambda/raw2staging.py
+++ scripts/lambda/raw2staging.py
@@ -1,26 +1,39 @@
 import boto3
 import os
+import gzip
+import shutil
 
-s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
+s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
+    print(event)
+    records = event['Records']
     source_bucket = os.environ['SOURCE_BUCKET']
     destination_bucket = os.environ['DESTINATION_BUCKET']
-
-    # List all objects in the source bucket
-    response = s3.list_objects_v2(Bucket=source_bucket)
-
-    # Loop through each object in the source bucket
-    if 'Contents' in response:
-        for obj in response['Contents']:
-            key = obj['Key']
-            copy_source = {'Bucket': source_bucket, 'Key': key}
-            dest_key = f"{os.path.splitext(os.path.basename(key))[0]}/{os.path.basename(key)}"
+    
+    for record in records:
+        key = record['s3']['object']['key']
+        copy_source = {'Bucket': source_bucket, 'Key': key}
+        
+        # if gz file, first gunzip, other file types will be just copied
+        if key.endswith('.gz'):
+            #first download to the tmp folder
+            local_gz_file = f'/tmp/{key.split("/")[-1]}'
+            s3_client.download_file(source_bucket, key, local_gz_file)
             
-            s3.copy_object(CopySource=copy_source, Bucket=destination_bucket, Key=dest_key)
-            print(f"Copied {key} to {dest_key} in bucket {destination_bucket}")
+            #unzip
+            local_csv_file = local_gz_file[:-3]
+            with gzip.open(local_gz_file, 'rb') as f_in:
+                with open(local_csv_file, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            
+            # upload unzip file
+            dest_key = f'{local_csv_file.split("/")[-1]}'
+            s3_client.upload_file(local_csv_file, destination_bucket, dest_key)
 
-    return {
-        'statusCode': 200,
-        'body': 'Files copied successfully'
-    }
+        else:
+            dest_key = f'{key.split("/")[-1]}' 
+            s3.meta.client.copy(copy_source, destination_bucket, dest_key)
+        
+        
